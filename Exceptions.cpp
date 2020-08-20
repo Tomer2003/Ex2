e525e79c4e2705c37323b5d11994082f58e2a930
tomer12901@gmail.com <tomer12901@gmail.com>
2020-08-20 21:53:26 +0300

    added exceptions

---
+++ Exceptions.cpp
@@ -0,0 +1,67 @@
+
+#include <iostream>
+#include "Exceptions.hpp"
+#include "ErrorCode.h"
+
+namespace Exceptions {
+
+class ErrorExceedMatrix {
+public:
+    void printException() {
+        std::cout << "ERROR: The column or row index given exceed the matrix boundaries!" << std::endl;
+    }
+};
+
+class ErrorCantAdd {
+public:
+    void printException() {
+        std::cout << "ERROR: The matrix sizes are not equal and therefore it is not possible to perform an add operation" << std::endl;
+    }
+};
+
+class ErrorCantMult {
+public:
+    void printException() {
+        std::cout << "ERROR: The number of rows in the left matrix is not equal to the number of columns in the right matrix and therefore it is not possible to perform a multiplication operation" << std::endl;
+    }
+};
+
+class ErrorNullMatrix {    
+public:
+    void printException() {
+        std::cout << "ERROR: The matrix that given is null!" << std::endl;
+    }
+};
+
+class ErrorCantAllocateMatrix {
+public:
+    void printException() {
+        std::cout << "Error: The memory allocation has failed" << std::endl;
+    }
+};
+
+class ErrorMatrixSizeNotAppopriate {
+public:
+    void printException() {
+        std::cout << "The height or width given in create matrix function to the matrix is 0!" << std::endl;
+    }
+};
+
+void throwAppopriateErrorIfHas(ErrorCode code){
+    switch (code) {
+		case EXCEED_MATRIX_SIZE_ERROR:
+			throw ErrorExceedMatrix();
+		case MATRIX_ADD_ERROR:
+			throw ErrorCantAdd();
+		case MATRIX_MULT_ERROR:
+			throw ErrorCantMult();
+		case NULL_ERROR:
+			throw ErrorNullMatrix();
+		case OUT_OF_MEMORY_ERROR:
+			throw ErrorCantAllocateMatrix();
+		case MATRIX_SIZE_NOT_APPOPRIATE:
+			throw ErrorMatrixSizeNotAppopriate();
+		default:
+	}
+}
+}
