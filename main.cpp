1fd5b658ce81775908d16ea57f63e06ed7e856ba
tomer12901@gmail.com <tomer12901@gmail.com>
2020-08-22 22:37:12 +0300

    added files

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+#include <iostream>
+#include "BitMap.hpp"
+#include "BitMapFactory.hpp"
+#include <iostream>
+
+int main() {
+ 
+std::cout << "hhh" << std::endl;
+//BitMap::bitMapAbstract* bitMap = Factory::BitMapFactory::fromFile("tsaban.bmp");
+std::unique_ptr<BitMap::bitMapAbstract> BitMap = std::make_unique<BitMap::bitMapAbstract>(Factory::BitMapFactory::fromFile("tsaban.bmp"));    
+/*if(typeid(*bitMap) == typeid(BitMap::bitMap8Bits)){
+    std::cout << "aaa";
+}
+else if(typeid(*bitMap) == typeid(BitMap::bitMap24Bits)){
+    std::cout << "bbb";
+}*/
+//std::cout << "hhh" << std::endl;
+//std::cout << "hhh" << std::endl;
+/*BitMap->rotate90Degrees();
+BitMap->convertToGray();
+BitMap->toFile("capara.bmp");
+
+*/
+return 0;
+}
