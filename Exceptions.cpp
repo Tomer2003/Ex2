0bf8bbd90df2d5d39ab320970422de39ed5fcfd8
tomer12901@gmail.com <tomer12901@gmail.com>
2020-08-23 17:08:55 +0300

    added to gitignore vscode

--- Exceptions.cpp
+++ Exceptions.cpp
@@ -38,9 +38,8 @@ void ErrorMatrixSizeNotAppopriate::printException(){
  * @param code - code to ErrorCode
  */
 void throwAppopriateErrorIfHas(ErrorCode code){
-	int a = code;
-	printf("%d", a);
-	/*switch (code) {
+	int errorCode = code;
+	switch (errorCode) {
 		case EXCEED_MATRIX_SIZE_ERROR:
 			throw ErrorExceedMatrix();
 		case MATRIX_ADD_ERROR:
@@ -53,6 +52,6 @@ void throwAppopriateErrorIfHas(ErrorCode code){
 			throw ErrorCantAllocateMatrix();
 		case MATRIX_SIZE_NOT_APPOPRIATE:
 			throw ErrorMatrixSizeNotAppopriate();
-		}*/
+		}
 	}
 }
